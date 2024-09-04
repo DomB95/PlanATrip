ca4bd52103dc38bf8428d40f3ee74c50a2c45f77
DomBie <drb0708@hotmail.com>
2024-09-04 15:36:56 -0400

    ProgressOnMenuScreen

--- app/src/main/java/com/example/planatrip/MainMenuScreen.java
+++ app/src/main/java/com/example/planatrip/MainMenuScreen.java
@@ -15,36 +15,5 @@ import java.util.Locale;
 
 public class MainMenuScreen extends AppCompatActivity{
 
-    private Button calbutton;
-    Calendar cal = Calendar.getInstance();
-    Button calbut;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.mainscreen);
-        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                cal.set(Calendar.YEAR, year);
-                cal.set(Calendar.MONTH, month);
-                cal.set(Calendar.DAY_OF_MONTH, day);
-                updateLabel();
-            }
-
-
-        };
-        calbut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new DatePickerDialog(MainMenuScreen.this,date,cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
-    }
-    private void updateLabel(){
-        String myFormat="MM/dd/yy";
-        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
-        calbut.setText(dateFormat.format(cal.getTime()));
-    }
 }
