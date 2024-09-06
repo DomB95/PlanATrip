3af3943d7b0bca46cc5871dd2c3d0e329b022f01
DomBie <drb0708@hotmail.com>
2024-09-06 17:52:20 -0400

    UpdateOnMainScreen.java

--- app/src/main/java/com/example/planatrip/MainMenuScreen.java
+++ app/src/main/java/com/example/planatrip/MainMenuScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,58 @@ public class MainMenuScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
 
+       Button planbutton = (Button) findViewById(R.id.plantripbutton);
+       planbutton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               Intent newscreen = new Intent(MainMenuScreen.this, PlanATrip.class);
+               startActivity(newscreen);
+
+           }
+       });
+       Button checkflightsbutton = (Button) findViewById(R.id.CheckFlightsbutton);
+       checkflightsbutton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+           }
+       });
+        Button hotelbutton = (Button) findViewById(R.id.checkhotelsbutton);
+        hotelbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button activitybutton = (Button) findViewById(R.id.actvivitiesbutton);
+        activitybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button foodbutton = (Button) findViewById(R.id.restaurantsbutton);
+        foodbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        Button itinaraybutton = (Button) findViewById(R.id.checktripsbutton);
+        itinaraybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
     }
+
 
 }
---
+++ app/src/main/java/com/example/planatrip/PlanATrip.java
@@ -0,0 +1,14 @@
+package com.example.planatrip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlanATrip extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tripstarting);
+
+    }
+
+}
