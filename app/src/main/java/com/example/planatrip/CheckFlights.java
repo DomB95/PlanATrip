466517da6ddea06e5e2ef085b3afea8fdaf0168a
DomBie <drb0708@hotmail.com>
2024-09-11 14:52:01 -0400

    working on flightscreen screen

--- app/src/main/java/com/example/planatrip/CheckFlights.java
+++ app/src/main/java/com/example/planatrip/CheckFlights.java
@@ -3,10 +3,13 @@ package com.example.planatrip;
 import android.app.Activity;
 import android.os.Bundle;
 import android.telephony.mbms.StreamingServiceInfo;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckFlights extends AppCompatActivity {
+
+    TextView cityname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,11 @@ public class CheckFlights extends AppCompatActivity {
         String tripname = SharedPref.read(SharedPref.TripName, null);
         String departcode = SharedPref.read(SharedPref.FromDes, null);
         String destinationcode = SharedPref.read(SharedPref.Destin, null);
+        String citydestination = SharedPref.read(SharedPref.CityName, null);
+
+        cityname = findViewById(R.id.destinationname);
+        cityname.setText(citydestination);
+
 
 
 
